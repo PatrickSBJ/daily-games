e8a565823990d217fc26b1d861c0e54ab26eb641
PatrickSBJ <patricksbjgd@gmail.com>
2021-10-29 15:32:29 -0500

    Created app components and organized file structure

---
+++ src/components/Game/Game.js
@@ -0,0 +1,10 @@
+
+// Hard coded example game for testing
+const game = {
+    developers: 'valve-software',
+    publishers: 'microsoft-studios',
+    genres: 'action',
+    tags: 'singleplayer',
+    creators: 'mike-morasky'
+    
+};
